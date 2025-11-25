a43aaff657d773ce61f81742336b36b0a6c6eca8
Malygos1016 <469539293@qq.com>
2025-11-25 09:56:51 +0800

    first commit

---
+++ GPM/GPM.cpp
@@ -0,0 +1,15 @@
+#include "GPM.h"
+
+//距喘議Demo
+double Add(double a, double b)
+{
+	return a + b;
+}
+
+/*！！！！！！！！！！！！
+Considering about what I need to do
+This is a Function file of GPM.dll
+GPM means General Pridict Model.
+
+The first part is
+！！！！！！！！！！！！*/
---
+++ GPM/GPM.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif // _cpp
+	__declspec(dllexport)
+		double Add(double a, double b);
+#ifdef __cplusplus
+}
+#endif // 
+
