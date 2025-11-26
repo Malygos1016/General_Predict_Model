60e058431c7fddc977f99b8816f25d6bf0ffe1db
Malygos1016 <469539293@qq.com>
2025-11-26 09:36:09 +0800

    添加了有关实现思路的注释

--- GPM/GPM.cpp
+++ GPM/GPM.cpp
@@ -11,5 +11,14 @@ Considering about what I need to do
 This is a Function file of GPM.dll
 GPM means General Pridict Model.
 
-The first part is
+This should have a few function,such as:
+CreateBuffer
+ResetBuffer
+CreateInterBuffer
+ResetInterBuffer 
+First Step:
+Predict(mod1\2\3\4 for different numbers of parameter) input origin data, out put array of parameters and different temperatrue.
+
+Second Step:
+Predict		input array of parameters in first step and temperature, out put arrays of parameters for different parameters.
 ！！！！！！！！！！！！*/
